a6b2e2a436ab5eab5a1dfbecdc1a8ad1f733f53c
luis <luis_angelde@hotmail.com>
2021-11-24 17:15:26 -0600

    Dato1

--- Masmenos/Program.cs
+++ Masmenos/Program.cs
@@ -6,6 +6,20 @@ namespace Masmenos
     {
         static void Main(string[] args)
         {
+            /*
+             * 
+             * Dada una matriz de números enteros, calcule las proporciones de sus elementos que son positivos , negativos y cero . 
+             * Imprima el valor decimal de cada fracción en una nueva línea con lugares después del decimal.
+
+Nota: este desafío presenta problemas de precisión. Los casos de prueba se escalan a seis lugares decimales, 
+aunque las respuestas tienen un error absoluto de hasta 10^4 son aceptables.
+
+Ejemplo: 
+arr = []
+Existen elementos, dos positivos, dos negativos y un cero. Sus proporciones son,  y . Los resultados se imprimen como:
+             * 
+             **/
+
             Console.WriteLine("Hello World!");
         }
     }
